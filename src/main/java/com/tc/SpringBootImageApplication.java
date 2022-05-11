d035b58bba4707e0c30ea5ee28be402238e685c8
TEPATEL <TEPATEL@TEPATEL-A40-D.in.oracle.com>
2022-05-12 00:34:02 +0530

    first commit

---
+++ src/main/java/com/tc/SpringBootImageApplication.java
@@ -0,0 +1,13 @@
+package com.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageApplication.class, args);
+	}
+
+}
